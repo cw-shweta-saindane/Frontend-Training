a3bafee7678c815a8d0f9e45abc92f04eb575353
Shweta Rajendra Saindane <126763704+shwetasaindane@users.noreply.github.com>
2023-03-16 10:37:31 +0530

    Update filterdata.js

--- filterdata.js
+++ filterdata.js
@@ -1,9 +1,9 @@
 const data = [
     { body: "cruiser", power: "150cc", seater: "7 seater"},
     { body: "retro", power: "200cc", seater: "5 seater"}, 
-    { body: "cruiser", power: "300cc", seater: "3 seater" },
-    { body: "sports", power: "150cc",seater: "5 seater" },
-    { body: "retro", power: "300cc", seater: "3 seater" } 
+    { body: "cruiser", power: "300cc", seater: "3 seater"},
+    { body: "sports", power: "150cc",seater: "5 seater"},
+    { body: "retro", power: "300cc", seater: "3 seater"} 
   ];
 
   function filterData(filterObj) {
@@ -15,5 +15,7 @@ const data = [
     return filteredData;
   }
 
+
 console.log(filterData({power:"300cc",seater:"5 seater"}))
 // console.log(filterData({body:"retro"}))
+
