9b4ddee937bc70d2cf690636a562a9db239a7088
Shweta Rajendra Saindane <126763704+shwetasaindane@users.noreply.github.com>
2023-03-17 10:30:16 +0530

    Update filterdata.js

--- filterdata.js
+++ filterdata.js
@@ -1,21 +1,30 @@
 const data = [
-    { body: "cruiser", power: "150cc", seater: "7 seater"},
-    { body: "retro", power: "200cc", seater: "5 seater"}, 
-    { body: "cruiser", power: "300cc", seater: "3 seater"},
-    { body: "sports", power: "150cc",seater: "5 seater"},
-    { body: "retro", power: "300cc", seater: "3 seater"} 
-  ];
+  { body: "cruiser", power: "150cc", seater: "7 seater"},
+  { body: "retro", power: "200cc", seater: "5 seater"},
+  { body: "cruiser", power: "300cc", seater: "3 seater"},
+  { body: "sports", power: "150cc", seater: "5 seater"},
+  { body: "retro", power: "300cc", seater: "3 seater"},
+  // { body: "retro", power: "300cc", seater: "5 seater"},
+];
+let inputData = {};
 
-  function filterData(filterObj) {
-    let filteredData = [];
-    for (let key in filterObj) {
-        filteredData = [...filteredData,...data.filter(item => item[key] === filterObj[key])];
+function filterData(filterObj) {
+  for (let key in filterObj) {
+    if (inputData[key]) {
+      delete filterObj[key];
+      delete inputData[key];
     }
-  
-    return filteredData;
   }
+  let filteredData = data; 
+  inputData = { ...inputData, ...filterObj };
+  for (let key in inputData) {
+    filteredData = filteredData.filter(item => item[key] === inputData[key]);
+  }
+  return Object.keys(inputData).length > 0 ? filteredData : [];
+}
 
+console.log(filterData({ power: "300cc" }));
+console.log(filterData({ body: "retro" }));
+console.log(filterData({ power: "300cc" }));
 
-console.log(filterData({power:"300cc",seater:"5 seater"}))
-// console.log(filterData({body:"retro"}))
 
